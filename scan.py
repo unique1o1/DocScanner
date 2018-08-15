143b9a93275ed0c2276d3f3b3e704f09ee8645dc
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-08-15 11:31:57 +0545

    checking cv2 version

--- scan.py
+++ scan.py
@@ -30,6 +30,11 @@ def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
     return resized
 
 
+def is_cv2():
+
+    return cv2.__version__.startswith('2.')
+
+
 # construct the argument parser and parse the arguments
 a = argparse.ArgumentParser()
 a.add_argument("-i", "--image", required=True,
@@ -43,8 +48,8 @@ cv2.imshow("asd", image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 gray = cv2.GaussianBlur(gray, (5, 5), 1)
 edged = cv2.Canny(gray, 75, 200)
-
-cv2.imshow("asd", edged)
-
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
