907f63534a1949d97d8caea4624a0efd6f41b060
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-08-15 11:15:31 +0545

    resize function added

--- scan.py
+++ scan.py
@@ -4,8 +4,38 @@ import numpy as np
 import argparse
 import cv2
 
+
+def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        # calculate the ratio of the height and construct the dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation=inter)
+
+    return resized
+
+
 # construct the argument parser and parse the arguments
 a = argparse.ArgumentParser()
 a.add_argument("-i", "--image", required=True,
                help="Path to the image to be scanned")
 args = a.parse_args()
+
+image = cv2.imread(args.image)
+orig = image.copy()
+image = resize(image, height=500)
