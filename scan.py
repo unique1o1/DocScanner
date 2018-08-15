8cafdf7469eb56c1a87a1288ee918da4771b2b13
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-08-15 11:59:56 +0545

    countor finding

--- scan.py
+++ scan.py
@@ -44,12 +44,37 @@ print(args.image)
 image = cv2.imread(args.image)
 orig = image.copy()
 image = resize(image, height=500)
-cv2.imshow("asd", image)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-gray = cv2.GaussianBlur(gray, (5, 5), 1)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(gray, 75, 200)
 print("STEP 1: Edge Detection")
-cv2.imshow("Image", image)
+cv2.imshow("Image", gray)
 cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+# find the contours in the edged image, keeping only the
+# largest ones, and initialize the screen contour
+cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if is_cv2() else cnts[1]
+cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
+
+# loop over the contours
+print(cnts)
+for c in cnts:
+    # approximate the contour
+    peri = cv2.arcLength(c, True)
+    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+
+    # if our approximated contour has four points, then we
+    # can assume that we have found our screen
+
+    if len(approx) == 4:
+        screenCnt = approx
+        break
+
+# show the contour (outline) of the piece of paper
+print("STEP 2: Find contours of paper")
+cv2.drawContours(image, [screenCnt], -1, (0, 255, 0), 2)
+cv2.imshow("Outline", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
